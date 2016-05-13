e847d3989ed7b0c6dfe49dc377ab05d057a7ff8d
wing328 <wing328hk@gmail.com>
2016-05-13 16:46:17 +0800

    Minor update

---
+++ category.go
@@ -0,0 +1,8 @@
+package swagger
+
+type Category struct {
+
+	Id int64 `json:"id,omitempty"`
+
+	Name string `json:"name,omitempty"`
+}
---
+++ tag.go
@@ -0,0 +1,8 @@
+package swagger
+
+type Tag struct {
+
+	Id int64 `json:"id,omitempty"`
+
+	Name string `json:"name,omitempty"`
+}
